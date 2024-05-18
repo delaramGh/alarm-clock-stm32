5d7e114fa85619249e047ea2c9813902e48cd978
DelaramGh <deli.zizou@gmail.com>
2024-05-17 23:23:47 -0400

    alarm clock finalized

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -145,13 +145,13 @@ int main(void)
 			sprintf(display_buffer, "! ALARM !     ");
 			//Alarm beeping
 			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_RESET);
-			HAL_Delay(500);
+			HAL_Delay(1250);
 			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_SET);
-			HAL_Delay(1000);
+			HAL_Delay(250);
 		}
 		else 
 		{
-			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_SET); //turns off the alarm
+			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_RESET); //turns off the alarm
 		}
 			
 	
@@ -291,7 +291,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL2;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -305,7 +305,7 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
   {
     Error_Handler();
   }
--- Core/Src/rtc.c
+++ Core/Src/rtc.c
@@ -46,9 +46,9 @@ void MX_RTC_Init(void)
   /* USER CODE BEGIN Check_RTC_BKUP */
 	if(HAL_RTCEx_BKUPRead(&hrtc, RTC_BKP_DR1) != 0x32F2)
 	{
-  /* USER CODE END Check_RTC_BKUP */
+		/* USER CODE END Check_RTC_BKUP */
 
-  /** Initialize RTC and set the Time and Date
+		/** Initialize RTC and set the Time and Date
 		*/
 		sTime.Hours = 11;
 		sTime.Minutes = 10;
